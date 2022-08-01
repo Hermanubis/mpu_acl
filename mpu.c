0a413587e7e67bcf54d2f6b8d9d843a435f4bcc3
Alexander Li <43350164+Hermanubis@users.noreply.github.com>
2022-07-31 20:19:34 -0700

    Add files via upload

---
+++ mpu.c
@@ -0,0 +1,7 @@
+#include "mpu.h"
+#include <stdio.h>
+
+#define NUM_SERVICE 3
+
+
+int soa_iam_acl(iam_t *iam, const char *client_name, const char *method);
---
+++ mpu.h
@@ -0,0 +1,13 @@
+#ifndef _mpu_H
+#define _mpu_H
+
+#include <stdbool.h>
+
+typedef struct iam_t {
+
+}iam_t;
+
+int soa_iam_acl(iam_t *iam, const char *client_name, const char *method);
+
+
+#endif
